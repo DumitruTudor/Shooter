72254a7caf782521223670c1cbbcbdb394126711
Tudor <macedon123@yahoo.com>
2023-11-05 03:07:29 +0100

    added code to level and learned about delta

--- scenes/Logo.gd
+++ scenes/Logo.gd
@@ -1,21 +1,24 @@
 extends Sprite2D
 
 var pos: Vector2 = Vector2.ZERO
-const speed: int = 10
+const speed: int = 200
 var test_scale: int = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pos = Vector2(100,200)
+	pos = Vector2(300,200)
 	position = pos
-	var test_rotation: int = 45
-	rotation_degrees = test_rotation
+#	var test_rotation: int = 45
+#	rotation_degrees = test_rotation
+
 	test_scale = 2
 	scale = Vector2(test_scale,test_scale)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pos.x += speed
+	pos.x += speed * delta
 	position = pos
-	test_scale += 1 
-	scale = Vector2(test_scale,test_scale)
+
+	#print(position.x)
+	#test_scale += 1
+	#scale = Vector2(test_scale,test_scale)
 	
 	
---
+++ scenes/levels/level.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var test_array: Array[String] = ["Test", "Hello", "stuff"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Logo.rotation_degrees = 90
+
+	for i in test_array:
+		print(i)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Logo.rotation_degrees += 200 * delta
+	
+	# flow
+	if ($Logo.position.x > 1000):
+		$Logo.pos.x = 0
